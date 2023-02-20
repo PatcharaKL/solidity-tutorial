5a8c4a0e2582d0afce9c9047ec63d0afe906a4b9
Patchara <patchara.pck@gmail.com>
2023-02-20 16:02:34 +0700

    MyAuction Contract

---
+++ contracts/MyAuction.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+import "./Auction.sol";
+
+contract MyAuction is Auction {
+    Product public myProduct;
+    mapping (address => uint) bids;
+    address [] bidders;
+
+    constructor(string memory brand, string memory serial, uint period) {
+        auctionOwner = payable(msg.sender); // address => address payable
+        auctionStart = block.timestamp;
+        auctionEnd = auctionStart + period * 1 hours;
+        STATE = AuctionState.STARTED;
+        myProduct = Product(brand, serial);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == auctionOwner);
+        _;
+    }
+
+    modifier onGoingAuction {
+        require(STATE == AuctionState.STARTED);
+        _;
+    }
+
+    function cancelAuction() public virtual override onlyOwner returns (bool) {}
+
+    function endAuction() public virtual override onlyOwner returns (bool) {}
+
+    function bid() public virtual override returns (bool) {}
+
+    function getStatus() public virtual override returns (int256) {}
+
+    function getProductInfo()
+        public
+        virtual
+        override
+        returns (string memory, string memory)
+    {}
+
+    function withdraw() public virtual override returns (bool) {}
+
+    function getMyBid(
+        address bidder
+    ) public virtual override returns (uint256) {}
+}
