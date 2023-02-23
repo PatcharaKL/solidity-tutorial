595d4d5b7f5aacf4ac6c39f074ae8b32aad4843a
Patchara <patchara.pck@gmail.com>
2023-02-23 23:12:44 +0700

    auction

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,10 +1,11 @@
 // const ConvertLib = artifacts.require("ConvertLib");
 // const MetaCoin = artifacts.require("MetaCoin");
-const BasicMath = artifacts.require("BasicMath")
-
-module.exports = function(deployer) {
+// const BasicMath = artifacts.require("BasicMath")
+const MyAuction = artifacts.require("MyAuction");
+const product = require("./Product.json");
+module.exports = function (deployer) {
   // deployer.deploy(ConvertLib);
   // deployer.link(ConvertLib, MetaCoin);
   // deployer.deploy(MetaCoin);
-  deployer.deploy(BasicMath)
+  deployer.deploy(MyAuction, product.brand, product.serial, product.period);
 };
